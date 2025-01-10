167de6217ee4e1dcd5227f88e30b25990262750c
Divyanshu Chaudhary <138017683+DivyanshuChaudharyOff@users.noreply.github.com>
2025-01-10 11:03:24 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <h6 className="text-sm text-center w-full">
+      {" "}
+      <a
+        href="https://github.com/DiavynshuChaudharyOff"
+        target="_blank"
+        alt="Divyanshu Chaudhary's GitHub Account"
+        className="hover:underline"
+      >
+      </a>
+    </h6>
+  );
+}
+
+export default Footer;
